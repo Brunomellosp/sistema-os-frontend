5e9183faa060047ebefc3d52d347f7e7cd6af19b
Bruno Melo <130388432+Brunomellosp@users.noreply.github.com>
2025-12-02 17:36:55 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'https://sigos-backend.fly.dev/api/v1'
-  // baseURL: 'http://localhost:8000/api/v1'
+	baseURL: 'https://sigos-backend.vercel.app/api/v1'
 });
 
 export default api;
